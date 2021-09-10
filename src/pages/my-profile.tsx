b95c3d5fb6580ac4ac2023540fbbfc49c444e36b
Anju <anju@centresource.in>
2021-09-10 18:06:11 +0530

    register, profile, warrenty

---
+++ src/pages/my-profile.tsx
@@ -0,0 +1,52 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import LayoutDefault from "../layout/Default";
+import styles from "../../styles/MyProfile.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <LayoutDefault>
+        <div className={styles.bghero}>
+          <div className="container">
+            <div className={styles.bgheroFrom}>
+              <div className={styles.profilerow}>
+                <div className={styles.row + " " + styles.profile}>
+                  <div className={styles.col5}>
+                    <h4>My Account</h4>
+                    <div className={styles.picEdit}>
+                      <div className={styles.proPic}>
+                        <img src="/pic.png"/>
+                      </div>
+                      <div className={styles.camIcon}>
+                        <a href="#"><img src="/icons/cam.png"/></a>
+                      </div>
+                    </div>
+                    <h2>Jonathan Doe</h2>
+                  </div>
+                  <div className={styles.col7}>
+                    <div className={styles.btn}>
+                      <a href="#"><button>Edit Profile</button></a>
+                    </div>
+                    <div className={styles.proDetails}>
+                      <p>Name : Jonathan Doe</p>
+                      <p>Mobile : +976 2564*****</p>
+                      <p className={styles.pmt20}>Email : jona****doe@gmail.com</p>
+                      <p>Password : *************<a href="#"><img src="/icons/edit.png"/></a></p> 
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.row}>
+                  <img src="/logobg.png"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutDefault>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/register.tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import LayoutDefault from "../layout/Default";
+import styles from "../../styles/Register.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <LayoutDefault>
+        <div className={styles.bghero}>
+          <div className="container">
+            <div className={styles.row}>
+              <div className={styles.col6}>
+                <div className={styles.bgheroFrom}>
+                  <div className={styles.heroFrom}>
+                    <h4>Create your account</h4>
+                    <div className={styles.row}>
+                      <div className={styles.col12}>
+                        <label>Name</label>
+                        <input type="text" placeholder="Jonathan Doe"/>
+                        <label>Email</label>
+                        <input type="email" placeholder="jonathandoe@gamil.com"/>
+                        <label>Mobile</label>
+                        <input type="text" placeholder="+976 256474473"/>
+                      </div>
+                    </div>
+                    <div className={styles.row}>
+                      <div className={styles.col6}>
+                        <label>Password</label>
+                        <input type="password" placeholder="****************"/>
+                      </div>
+                      <div className={styles.col6}>
+                        <label>Repeat Password</label>
+                        <input type="password" placeholder="****************"/>
+                      </div>
+                    </div>
+                    <div className={styles.row}>
+                      <div className={styles.col12}>
+                        <input type="submit" value="Sign Up" />
+                      </div>
+                      <div className={styles.col12}>
+                        <p><a>Sign in instead</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.col6}>
+                <div className={styles.titleBox}>
+                  <h6>Welcome to</h6>
+                  <h1>Stratos Ceramica</h1>
+                  <h5>Luxury Redefined</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutDefault>
+    </>
+  );
+};
+
+export default Home;
