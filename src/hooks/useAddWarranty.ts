d7870090a480d7463aecb49195d176c216f35b50
Edwin Manual <edwn.mnl@gmail.com>
2021-09-19 21:07:02 +0530

    Add react html parser types

--- src/hooks/useAddWarranty.ts
+++ src/hooks/useAddWarranty.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 function useAddWarranty() {
   const URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/warranty-registrations`;
 
-  return useMutation((warranty) => axios.post(URL, warranty));
+  return useMutation((warranty: any) => axios.post(URL, warranty));
 }
 
 export default useAddWarranty;
