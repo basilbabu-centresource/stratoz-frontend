d8662d7abaf698374fd4b6ec40e77e74b287601c
Edwin Manual <edwin.manaul@centresource.in>
2021-09-24 15:36:01 +0530

    Add page loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,14 @@ import { validateToken } from "../features/auth/authSlice";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import api from "../api";
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const queryClient = new QueryClient();
 
