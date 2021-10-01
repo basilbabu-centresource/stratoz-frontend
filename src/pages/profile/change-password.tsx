6b65615b010d9d71d3f1a0021178a40d877d67f3
Anju <anju@centresource.in>
2021-10-01 18:15:35 +0530

    api issue testing and fixing

--- src/pages/profile/change-password.tsx
+++ src/pages/profile/change-password.tsx
@@ -6,7 +6,7 @@ import styles from "../../../styles/EditProfile.module.scss";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/auth/authSlice";
 import FormButton from "../../components/FormButton";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { toast } from "react-toastify";
 import api from "../../api";
 
@@ -25,9 +25,8 @@ const ChangePassword: NextPage = () => {
     reset,
     formState: { errors },
   } = useForm<Inputs>();
-
+  
   const handleEditProfile = (data: any) => {
-    console.log(data);
 
     const userData = {
       password: data.newPassword,
@@ -38,12 +37,19 @@ const ChangePassword: NextPage = () => {
     api
       .post("/auth/change-my-password", userData)
       .then(() => {
-        toast.success("Profile edited successfully");
+        toast.success("Password changed successfully");
       })
       .catch((error) => {
         console.log("Error: ", error.response.data);
 
         if (error.response.data) {
+          
+          if(error.response.data.current_password){
+            toast.error(error.response.data.current_password[0]);
+          }
+          if(error.response.data.password){
+            toast.error(error.response.data.password[0]);
+          }
           // error.response.data.message[0].messages.map((message: any) => {
           //   toast.error(message.message);
           // });
@@ -66,20 +72,39 @@ const ChangePassword: NextPage = () => {
                         <label>Current Password</label>
                         <input
                           type="password"
-                          {...register("currentPassword", { required: false })}
+                          {...register("currentPassword", { required: true })}
                         />
+                        <span className="error">
+                          {errors.currentPassword && errors.currentPassword.type === "required" && (
+                            <span>Current Password is required</span>
+                          )}
+                        </span>
+                      </div>
+                      <div className={styles.col12}>
                         <label>New password</label>
                         <input
                           type="password"
-                          {...register("newPassword", { required: false })}
+                          {...register("newPassword", { required: true })}
                         />
+                        <span className="error">
+                          {errors.newPassword && errors.newPassword.type === "required" && (
+                            <span>New Password is required</span>
+                          )}
+                        </span>
+                      </div>
+                      <div className={styles.col12}>
                         <label>Confirm new password</label>
                         <input
                           type="password"
                           {...register("confirmNewPassword", {
-                            required: false,
+                            required: true,
                           })}
                         />
+                        <span className="error">
+                          {errors.confirmNewPassword && errors.confirmNewPassword.type === "required" && (
+                            <span>Confirm Password is required</span>
+                          )}
+                        </span>
                       </div>
                     </div>
                     <div className={styles.row}>
